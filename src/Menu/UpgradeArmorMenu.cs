85e8e5c22f295e30855e4f935ec1f37d0aafd67b
Surname-TK <miniluigimightyfive@gmail.com>
2024-11-16 19:21:58 -0400

    antidive renaming

--- src/Menu/UpgradeArmorMenu.cs
+++ src/Menu/UpgradeArmorMenu.cs
@@ -246,9 +246,9 @@ public class UpgradeArmorMenu : IMainMenu {
 		var gameMode = level.gameMode;
 		DrawWrappers.DrawTextureHUD(Global.textures["pausemenu"], 0, 0);
 		string armorName = xGame switch {
-			1 => "First Armor",
-			2 => "Second Armor",
-			3 => "Third Armor",
+			1 => "Light Armor",
+			2 => "Giga Armor",
+			3 => "Max Armor",
 			_ => "ERROR"
 		};
 		Fonts.drawText(
